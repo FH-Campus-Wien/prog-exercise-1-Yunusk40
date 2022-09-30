38553ee142eee6ab5f968aad9f669fc68013bb9d
yunuskara <yunuskr40@gmail.com>
2022-09-30 15:35:55 +0200

    Task 1 solved

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -1,92 +1,124 @@
 package at.ac.fhcampuswien;
 
-import java.util.Scanner;
-
 public class App {
 
     //todo Task 1
-    public void sayHelloWorld(){
+    public void sayHelloWorld() {
         // input your solution here
         System.out.println("Hello World!");
     }
 
     //todo Task 2
-    public void helloRobot(){
+    public void helloRobot() {
         // input your solution here
+        System.out.println("0123456789012345678901" + System.lineSeparator() +
+                "         __" + System.lineSeparator() +
+                " _(\\    |@@|" + System.lineSeparator() +
+                "(__/\\__ \\--/ __" + System.lineSeparator() +
+                "   \\___|----|  |   __" + System.lineSeparator() +
+                "       \\ }{ /\\ )_ / _\\" + System.lineSeparator() +
+                "       /\\__/\\ \\__O (__" + System.lineSeparator() +
+                "      (--/\\--)    \\__/" + System.lineSeparator() +
+                "      _)(  )(_" + System.lineSeparator() +
+                "     `---''---`");
     }
 
-    //todo Task 3
-    public void sumOfLiterals(){
-        // input your solution here
+        //todo Task 3
+        public void sumOfLiterals () {
+            // input your solution here
+            System.out.println("");
+            int sum;
+            char z = 'Z';
+            int a = 0xface;
+            int b = 012;
+            long c = 80L;
+            float d = 44e-1f;
+            double e = 5.5f;
+            double f = 8.88e1;
+            double g = 99.9;
+
+            int zz = z;
+            int cc = (int) c;
+            int dd = (int) d;
+            int ee = (int) e;
+            int ff = (int) f;
+            int gg = (int) g;
+
+            sum = zz+a+b+cc+dd+ee+ff+gg;
+            System.out.println(sum);
+        }
+
+        //todo Task 4
+        public void addTwoNumbers () {
+            // input your solution here
+            import java.util.Scanner;
+
+            public class scanner
+
+        }
+
+        //todo Task 5
+        public void swapTwoNumbers () {
+            // input your solution here
+        }
+
+        //todo Task 6
+        public void compareTwoNumbers () {
+            // input your solution here
+        }
+
+        //todo Task 7
+        public void ratingSalesPerson () {
+            // input your solution here
+        }
+
+        //todo Task 8
+        public void getCommissionRate () {
+            // input your solution here
+        }
+
+        //todo Task 9
+        public void leapyear () {
+            // input your solution here
+        }
+
+        //todo Task 10
+        public void transposedNumbers () {
+            // input your solution here
+        }
+
+
+        public static void main (String[]args){
+            App exercise1 = new App();
+
+            System.out.println("Task 1: Say Hello World");
+            exercise1.sayHelloWorld();
+
+            System.out.println("\nTask 2: Hello Robot");
+            exercise1.helloRobot();
+
+            System.out.println("\nTask 3: Literals");
+            exercise1.sumOfLiterals();
+
+            System.out.println("\nTask 4: Add two Numbers");
+            exercise1.addTwoNumbers();
+
+            System.out.println("\nTask 5: Swap two Numbers without using a helper variable");
+            exercise1.swapTwoNumbers();
+
+            System.out.println("\nTask 6: Compare two Numbers");
+            exercise1.compareTwoNumbers();
+
+            System.out.println("\nTask 7: Rating Sales Person");
+            exercise1.ratingSalesPerson();
+
+            System.out.println("\nTask 8: Commission Rate");
+            exercise1.getCommissionRate();
+
+            System.out.println("\nTask 9: Leapyear");
+            exercise1.leapyear();
+
+            System.out.println("\nTask 10: Transposed Numbers");
+            exercise1.transposedNumbers();
+        }
     }
-
-    //todo Task 4
-    public void addTwoNumbers(){
-        // input your solution here
-    }
-
-    //todo Task 5
-    public void swapTwoNumbers(){
-        // input your solution here
-    }
-
-    //todo Task 6
-    public void compareTwoNumbers(){
-        // input your solution here
-    }
-
-    //todo Task 7
-    public void ratingSalesPerson(){
-        // input your solution here
-    }
-
-    //todo Task 8
-    public void getCommissionRate(){
-        // input your solution here
-    }
-
-    //todo Task 9
-    public void leapyear(){
-        // input your solution here
-    }
-
-    //todo Task 10
-    public void transposedNumbers(){
-        // input your solution here
-    }
-
-
-    public static void main(String[] args){
-        App exercise1 = new App();
-
-        System.out.println("Task 1: Say Hello World");
-        exercise1.sayHelloWorld();
-
-        System.out.println("\nTask 2: Hello Robot");
-        exercise1.helloRobot();
-
-        System.out.println("\nTask 3: Literals");
-        exercise1.sumOfLiterals();
-
-        System.out.println("\nTask 4: Add two Numbers");
-        exercise1.addTwoNumbers();
-
-        System.out.println("\nTask 5: Swap two Numbers without using a helper variable");
-        exercise1.swapTwoNumbers();
-
-        System.out.println("\nTask 6: Compare two Numbers");
-        exercise1.compareTwoNumbers();
-
-        System.out.println("\nTask 7: Rating Sales Person");
-        exercise1.ratingSalesPerson();
-
-        System.out.println("\nTask 8: Commission Rate");
-        exercise1.getCommissionRate();
-
-        System.out.println("\nTask 9: Leapyear");
-        exercise1.leapyear();
-
-        System.out.println("\nTask 10: Transposed Numbers");
-        exercise1.transposedNumbers();
-    }
-}
